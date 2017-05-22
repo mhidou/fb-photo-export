53cf4a9a9d9e0b696432d29e70512a8c8d71c911
Mehdi AHRAOUI <mehdi@mhidou.io>
2017-05-22 04:12:57 +0000

    adds albumsList

--- front/src/components/Album/index.js
+++ front/src/components/Album/index.js
@@ -1,8 +1,50 @@
 import React, { Component } from 'react';
+import moment from 'moment';
 
 class Album extends Component {
+  state = {
+    background: ''
+  }
+
+  componentDidMount () {
+    const self = this;
+    window.FB.api(
+        '/' + this.props.id + '?fields=cover_photo',
+        function (response) {
+          if (response && !response.error) {
+            self.getBackgroundImage(response.cover_photo.id);
+          }
+        }
+    );
+  }
+
+  getBackgroundImage(id) {
+    const self = this;
+    window.FB.api(
+        '/' + id + '?fields=images',
+        function (response) {
+          console.log(response);
+          if (response && !response.error) {
+            self.setState({
+              background: response.images[0].source
+            });
+          }
+        }
+    );
+  }
+
   render() {
-    return <div>Album</div>
+    const {name, created_time} = this.props
+    return (
+      <div style={{backgroundImage:'url(' + this.state.background + ')'}}>
+        <span>
+          {name}
+        </span>
+        <span>
+          Created {moment(created_time).fromNow()}
+        </span>
+      </div>
+    )
   }
 }
 
--- front/src/components/AlbumsList/index.js
+++ front/src/components/AlbumsList/index.js
@@ -1,8 +1,27 @@
 import React, { Component } from 'react';
+import Album from '../Album'
 
 class AlbumsList extends Component {
+
+  constructor(props) {
+    super(props);
+    console.log(props.me.albums);
+    this.state = {
+      albums: props.me.albums.data,
+      after: props.me.albums.paging.cursors.after,
+      before: props.me.albums.paging.cursors.before
+    };
+  }
+
   render() {
-    return <div>AlbumsList</div>
+    const albums = this.state.albums;
+    return (
+      <div>
+        {albums.map(function(elt, index){
+          return <Album key={index} id={elt.id} name={elt.name} created_time={elt.created_time} />;
+        })}
+      </div>
+    )
   }
 }
 
